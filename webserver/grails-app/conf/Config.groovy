669bed62688bae0f9170cd08a75f848de03eea6c
jimbitol <cabreram.815@gmail.com>
2015-11-25 23:14:01 -0300

    test

--- webserver/grails-app/conf/Config.groovy
+++ webserver/grails-app/conf/Config.groovy
@@ -91,7 +91,7 @@ environments {
     }
     production {
         grails.logging.jul.usebridge = false
-        grails.serverURL = "http://127.0.0.1:8080/${appName}"
+        //grails.serverURL = "http://127.0.0.1:8080/${appName}"
     }
 }
 
