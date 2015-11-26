e6dc23180d7918f6695aa7bcb20f5ff35ee67479
jimbitol <cabreram.815@gmail.com>
2015-11-25 23:36:47 -0300

    test

--- webserver/grails-app/conf/UrlMappings.groovy
+++ webserver/grails-app/conf/UrlMappings.groovy
@@ -1,13 +1,15 @@
 class UrlMappings {
 
 	static mappings = {
+
+        "/"(view:"/index")
+
+        "500"(view:'/error')
+
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
                 // apply constraints here
             }
         }
-
-        "/"(view:"/index")
-        "500"(view:'/error')
 	}
 }
