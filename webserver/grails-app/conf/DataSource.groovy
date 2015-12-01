b3ae6611f1e45d423cbf98be5b68f11405b3ec55
jimbitol <cabreram.815@gmail.com>
2015-11-30 21:55:51 -0300

    test

--- webserver/grails-app/conf/DataSource.groovy
+++ webserver/grails-app/conf/DataSource.groovy
@@ -2,6 +2,8 @@ hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
     cache.region.factory_class = 'org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory' // Hibernate 4
+    singleSession = true // configure OSIV singleSession mode
+    flush.mode = 'manual' // OSIV session flush mode outside of transactional context
 }
 
 // environment specific settings
